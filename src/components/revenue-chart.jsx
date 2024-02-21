22147edfb098e7dc7f57a7926879ef1c24457976
Mykola Slepko <slepko13@gmail.com>
2024-02-21 21:35:11 +0200

    init

---
+++ src/components/revenue-chart.jsx
@@ -0,0 +1,69 @@
+import { generateYAxis } from "../lib/utils";
+import { CalendarIcon } from "@heroicons/react/24/outline";
+
+const revenue = [
+  { month: "Jan", revenue: 2000 },
+  { month: "Feb", revenue: 1800 },
+  { month: "Mar", revenue: 2200 },
+  { month: "Apr", revenue: 2500 },
+  { month: "May", revenue: 2300 },
+  { month: "Jun", revenue: 3200 },
+  { month: "Jul", revenue: 3500 },
+  { month: "Aug", revenue: 3700 },
+  { month: "Sep", revenue: 2500 },
+  { month: "Oct", revenue: 2800 },
+  { month: "Nov", revenue: 3000 },
+  { month: "Dec", revenue: 4800 },
+];
+
+export default function RevenueChart() {
+  const chartHeight = 350;
+
+  const { yAxisLabels, topLabel } = generateYAxis(revenue);
+
+  if (!revenue || revenue.length === 0) {
+    return <p className="mt-4 text-gray-400">No data available.</p>;
+  }
+
+  return (
+    <div className="w-full md:col-span-4">
+      <h2 className="mb-4 text-xl md:text-2xl">Recent Revenue</h2>
+
+      <div className="rounded-xl bg-gray-50 p-4">
+        <div className=" mt-0 flex   rounded-md bg-white ">
+          <div
+            className="mb-6 hidden flex-col justify-between text-sm text-gray-400 sm:flex"
+            style={{ height: `${chartHeight}px` }}
+          >
+            {yAxisLabels.map((label) => (
+              <p key={label}>{label}</p>
+            ))}
+          </div>
+
+          <div className="flex items-end justify-between w-full mx-4">
+            {revenue.map((month) => (
+              <div
+                key={month.month}
+                className="flex flex-col justify-center basis-full mx-1"
+              >
+                <div
+                  className="w-full rounded-md bg-blue-300 mb-2"
+                  style={{
+                    height: `${(chartHeight / topLabel) * month.revenue}px`,
+                  }}
+                ></div>
+                <p className="-rotate-90 text-sm text-gray-400 sm:rotate-0 text-center">
+                  {month.month}
+                </p>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="flex items-center pb-2 pt-6 ">
+          <CalendarIcon className="h-5 w-5 text-gray-500" />
+          <h3 className="ml-2 text-sm text-gray-500 ">Last 12 months</h3>
+        </div>
+      </div>
+    </div>
+  );
+}
